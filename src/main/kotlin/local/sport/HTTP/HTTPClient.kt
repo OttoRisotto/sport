632e398e6ea84b040b8c04f5468e45ea1096ca0b
bandit <bandit@mail.com>
2026-04-21 10:03:46 +0200

    eigener Http Client und Server funktionieren

---
+++ src/main/kotlin/local/sport/HTTP/HTTPClient.kt
@@ -0,0 +1,31 @@
+package local.sport.HTTP
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+fun main(){
+    val client: myHttpClient = myHttpClient()
+    client.get();
+}
+
+
+class myHttpClient() {
+    private val client = HttpClient.newBuilder().build()
+    private val req = myRequest();
+
+    fun get(url:String="http://localhost:8080/"){
+        val response = this.client.send(this.req.get(), HttpResponse.BodyHandlers.ofString())
+        println(response.body())
+    }
+}
+
+private class myRequest(){
+    val request = HttpRequest.newBuilder()
+
+    fun get(urlParam:String = "http://localhost:8080/"): HttpRequest{
+        val url= URI.create( urlParam )
+        return request.GET().uri(url).build()
+    }
+}
--- src/main/kotlin/local/sport/HTTP/HttpServer.kt
+++ src/main/kotlin/local/sport/HTTP/HttpServer.kt
@@ -1,17 +1,18 @@
-package local.sport
+package local.sport.HTTP
 
 import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
 import com.sun.net.httpserver.HttpServer
 import java.net.InetSocketAddress
 
 fun main(){
-    val server = HttpServer.create(InetSocketAddress("127.0.0.1", 8000), 0)
+    val server = HttpServer.create(InetSocketAddress("127.0.0.1", 8080), 0)
     val h1 = myHandler()
     server.createContext("/", h1)
     server.start()
 }
 
-class myHandler: com.sun.net.httpserver.HttpHandler{
+class myHandler: HttpHandler{
     override fun handle(exchange: HttpExchange?) {
         val payload = "this is se payload"
         exchange?.sendResponseHeaders(200, payload.length.toLong())
