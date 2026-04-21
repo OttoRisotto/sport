c1908fa47f0249ddb936d0352e280f02279ff72f
bandit <bandit@mail.com>
2026-04-21 07:43:04 +0200

    fix1

--- src/test/kotlin/local/sport/HttpServer.kt
+++ src/test/kotlin/local/sport/HttpServer.kt
@@ -2,15 +2,11 @@ package local.sport
 
 import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpServer
-import org.springframework.http.server.reactive.HttpHandler
-import org.springframework.http.server.reactive.ServerHttpRequest
-import org.springframework.http.server.reactive.ServerHttpResponse
 import java.net.InetSocketAddress
 
 fun main(){
     val server = HttpServer.create(InetSocketAddress("127.0.0.1", 8000), 0)
     val h1 = myHandler()
-
     server.createContext("/", h1)
     server.start()
 }
