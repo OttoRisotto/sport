d174621c0c2e4fae54f02d58fb12fcfb42bb98eb
bandit <bandit@mail.com>
2026-04-28 20:40:28 +0200

    http yeah

--- src/main/kotlin/local/sport/HTTP/HTTPClient.kt
+++ src/main/kotlin/local/sport/HTTP/HTTPClient.kt
@@ -8,8 +8,10 @@ import java.net.http.HttpResponse
 fun main(){
     val client: myHttpClient = myHttpClient()
 
-    val url: String = "http://localhost:8080/"
-    client.get(url);
+    val localhost: String = "http://localhost:8080/"
+    val url: String = "http://www.example.com/"
+
+    client.get(localhost);
 
 }
 
@@ -19,7 +21,7 @@ class myHttpClient() {
 
     fun get(url:String){
         println("\nGet-Anfrage an $url startet\n")
-        val response = this.client.send(this.req.get(), HttpResponse.BodyHandlers.ofString())
+        val response = this.client.send(this.req.get(url), HttpResponse.BodyHandlers.ofString())
         println("Response: ${response}")
         println("Response-Body: ${response.body()}")
     }
@@ -28,7 +30,7 @@ class myHttpClient() {
 private class myRequest(){
     val request = HttpRequest.newBuilder()
 
-    fun get(urlParam:String = "http://localhost:8080/"): HttpRequest{
+    fun get(urlParam:String): HttpRequest{
         val url= URI.create( urlParam )
         return request.GET().uri(url).build()
     }
