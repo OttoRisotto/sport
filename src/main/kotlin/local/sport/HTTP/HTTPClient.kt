31f2492ad67d9581cb64f20e76a21c43666a769c
bandit <bandit@mail.com>
2026-04-29 21:55:38 +0200

    EventController.kt: HTTP-Methoden implementiert

--- src/main/kotlin/local/sport/HTTP/HTTPClient.kt
+++ src/main/kotlin/local/sport/HTTP/HTTPClient.kt
@@ -6,10 +6,10 @@ import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
 fun main(){
-    val client: myHttpClient = myHttpClient()
+    val client = myHttpClient()
 
-    val localhost: String = "http://localhost:8080/"
-    val url: String = "http://www.example.com/"
+    val localhost = "http://localhost:8080/events"
+    val url = "http://www.example.com/"
 
     client.get(localhost);
 
@@ -17,21 +17,30 @@ fun main(){
 
 class myHttpClient() {
     private val client = HttpClient.newBuilder().build()
-    private val req = myRequest();
+    private val req = myHttpRequest();
 
     fun get(url:String){
         println("\nGet-Anfrage an $url startet\n")
         val response = this.client.send(this.req.get(url), HttpResponse.BodyHandlers.ofString())
         println("Response: ${response}")
-        println("Response-Body: ${response.body()}")
+        println("Response-Body:\n${response.body()}")
     }
 }
 
-private class myRequest(){
-    val request = HttpRequest.newBuilder()
+private class myHttpRequest{
+    //erstellt nur die Requests
 
     fun get(urlParam:String): HttpRequest{
         val url= URI.create( urlParam )
-        return request.GET().uri(url).build()
+        return HttpRequest.newBuilder()
+            .uri(url)
+            .GET()
+            .build()
     }
+
+    /* fun post(urlParam: String, list: ArrayList<Any>): HttpRequest{
+        val url = URI.create( urlParam )
+        val body
+        return request.POST().uri(url).build()
+    }*/
 }
