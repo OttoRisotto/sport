fa3d1a79a8567ed2a43b6b147cedd1aa65da79d6
bandit <bandit@mail.com>
2026-06-03 13:24:40 +0200

    EventController: @PutMapping hier explizites @RequestParam hinzugefügt, dieser Queryparameter wurde vorher implizit, ohne @RequestParam eingelesen, explizit == besser yeah

--- src/main/kotlin/local/sport/Controllers/EventController.kt
+++ src/main/kotlin/local/sport/Controllers/EventController.kt
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.client.HttpStatusCodeException
@@ -49,13 +50,15 @@ class EventController (private val repo: EventRepository){
 
     @PutMapping("/events/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun updateEventByID(@PathVariable("id") id: UUID, newName: String){
+    fun updateEventByID ( @PathVariable("id") id: UUID, @RequestParam("newName") newName:String ) {/*Query Parameter
+        1. Spring Boot interpretiert unannotierte Parameter mit einfachen Datentypen (wie String) standardmäßig als Query-Parameter. Der URL-Zusatz ?newName=... wird automatisch dem Parameter newName zugewiesen.
+        2. explizit wäre: fun updateEventByID(@PathVariable("id") id: UUID, @RequestParam("newName") newName: String)
+       */
         val event: Event = repo.getEventByIdOrNull(id)
             ?: throw ResponseStatusException(
                 HttpStatus.NOT_FOUND,
                 "Event mit id $id nicht gefunden"
             )
-
         event.name = newName
         repo.save(event)
     }
@@ -71,4 +74,3 @@ class EventController (private val repo: EventRepository){
         repo.delete(event)
     }
 }
-
